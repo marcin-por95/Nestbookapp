a727c9bbcebc43bbacf8138f6b2711c0cddc0278
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-10-31 15:57:22 +0100

    Add delete endpoints and ts configuration

--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,50 +1,44 @@
 import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/shared/services/prisma.service';
-// @ts-ignore
-import { Author } from '@prisma/client';
+import { Author, Prisma } from '@prisma/client';
 
 @Injectable()
 export class AuthorsService {
     constructor(private prismaService: PrismaService) {}
 
-    public getAll(): Promise<Author[]> {
-        // @ts-ignore
+    public async getAll(): Promise<Author[]> {
         return this.prismaService.author.findMany();
     }
 
-    public getById(id: Author['id']): Promise<Author | null> {
-        // @ts-ignore
+    public async getById(id: string): Promise<Author | null> {
         return this.prismaService.author.findUnique({
-            where: { id },
+            where: {id},
         });
     }
 
-    public async create(authorData: Omit<Author, 'id'>): Promise<Author> {
+    public async create(authorData: Prisma.AuthorCreateInput): Promise<Author> {
         try {
-            // @ts-ignore
             return await this.prismaService.author.create({
                 data: authorData,
             });
         } catch (error) {
-            if (error.code === 'P2002')
+            if (error.code === 'P2002') {
                 throw new ConflictException('Name is already taken');
+            }
             throw error;
         }
     }
 
-    public async updateById(
-        id: Author['id'],
-        authorData: Omit<Author, 'id'>,
-    ): Promise<Author> {
-        // @ts-ignore
+    public async updateById(id: string, authorData: Prisma.AuthorUpdateInput): Promise<Author> {
         return this.prismaService.author.update({
-            where: { id },
+            where: {id},
             data: authorData,
         });
     }
-    catch(error) {
-        if (error.code === 'P2002')
-            throw new ConflictException('Name is already taken');
-        throw error;
+
+    public async deleteById(id: string): Promise<Author> {
+        return this.prismaService.author.delete({
+            where: {id},
+        });
     }
 }
